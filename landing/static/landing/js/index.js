46d6463c44ad91fbd5b9bd13bcb0e2fcfc4766a4
yiju <kls9939@naver.com>
2022-05-13 00:44:28 +0900

    CSS 스타일 정리정돈 및 자바스크립트 리팩토링

--- landing/static/landing/js/index.js
+++ landing/static/landing/js/index.js
@@ -1,90 +1,51 @@
-// 탭 버튼 기능 리팩토링 하기
+// 계산기 Tab 기능 구현
 
-const tab0 = document.querySelectorAll(".tab-item")[0];
-const tab1 = document.querySelectorAll(".tab-item")[1];
-const tab2 = document.querySelectorAll(".tab-item")[2];
+const tab = document.querySelectorAll(".tab-item");
+const tabContent = document.querySelectorAll(".tab-content");
 
-const tabContent0 = document.querySelectorAll(".tab-content")[0];
-const tabContent1 = document.querySelectorAll(".tab-content")[1];
-const tabContent2 = document.querySelectorAll(".tab-content")[2];
+// tab item과 tab content의 클래스 모두 제거해준 뒤
+// 클릭한 tab의 item과 content에 클래스 추가해주는 함수
+function tabOpen(param) {
+  for (var i = 0; i < tab.length; i++) {
+    tab[i].classList.remove("active");
+    tabContent[i].classList.remove("show");
+  }
+  tab[param].classList.add("active");
+  tabContent[param].classList.add("show");
+}
 
-function tabOpen0() {
-  tab0.classList.remove("active");
-  tab1.classList.remove("active");
-  tab2.classList.remove("active");
-  tabContent0.classList.remove("show");
-  tabContent1.classList.remove("show");
-  tabContent2.classList.remove("show");
-  tab0.classList.add("active");
-  tabContent0.classList.add("show");
-}
-function tabOpen1() {
-  tab0.classList.remove("active");
-  tab1.classList.remove("active");
-  tab2.classList.remove("active");
-  tabContent0.classList.remove("show");
-  tabContent1.classList.remove("show");
-  tabContent2.classList.remove("show");
-  tab1.classList.add("active");
-  tabContent1.classList.add("show");
-}
-function tabOpen2() {
-  tab0.classList.remove("active");
-  tab1.classList.remove("active");
-  tab2.classList.remove("active");
-  tabContent0.classList.remove("show");
-  tabContent1.classList.remove("show");
-  tabContent2.classList.remove("show");
-  tab2.classList.add("active");
-  tabContent2.classList.add("show");
-}
-tab0.addEventListener("click", function () {
-  tab1.classList.remove("active");
-  tab2.classList.remove("active");
-  tabContent1.classList.remove("show");
-  tabContent2.classList.remove("show");
-  tab0.classList.add("active");
-  tabContent0.classList.add("show");
+tab[0].addEventListener("click", () => {
+  tabOpen(0);
 });
-tab1.addEventListener("click", function () {
-  tab0.classList.remove("active");
-  tab2.classList.remove("active");
-  tabContent0.classList.remove("show");
-  tabContent2.classList.remove("show");
-  tab1.classList.add("active");
-  tabContent1.classList.add("show");
+tab[1].addEventListener("click", () => {
+  tabOpen(1);
 });
-tab2.addEventListener("click", function () {
-  tab1.classList.remove("active");
-  tab0.classList.remove("active");
-  tabContent1.classList.remove("show");
-  tabContent0.classList.remove("show");
-  tab2.classList.add("active");
-  tabContent2.classList.add("show");
+tab[2].addEventListener("click", () => {
+  tabOpen(2);
 });
 
-// 만약에 현재 tab-item이 0번에 있다면 ( 상태를 표현 해줘야함 )
-// 다음 버튼 클릭시 상태를 1로 바꾸고 탭2 를 보여준다.
+
+
+
+
+// 계산기 form 다음 버튼 기능 구현
+
 const calFirst = document.getElementById("calculator-first");
 const calSecond = document.getElementById("calculator-second");
 
-calFirst.addEventListener("click", tabOpen1);
-calSecond.addEventListener("click", tabOpen2);
+calFirst.addEventListener("click", () => {
+  tabOpen(1);
+});
+calSecond.addEventListener("click", () => {
+  tabOpen(2);
+});
 
-// const form = document.querySelector("form");
-// var select = document.getElementById("family-number");
-// var text = select.options[select.selectedIndex].text;
-// console.log(text);
 
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   nickName = document.getElementById("nickname").value;
 
-//   // console.log(familyNumberVal);
-// });
+
 
 // 숫자에 , 로 구분해주는 구성
-// this로 리팩토링하기
+// 리팩토링하기
 let workElement = document.getElementById("work");
 let cashElement = document.getElementById("cash");
 let houseElement = document.getElementById("house");
@@ -93,32 +54,34 @@ let medicalElement = document.getElementById("medical");
 let carElement = document.getElementById("car");
 let debtElement = document.getElementById("debt");
 
-function changeValue(stringNumber,element) {
-  let tempString = stringNumber.replaceAll(",", "");
-  let numberCash = BigInt(tempString);
-  element.value = numberCash
-    .toLocaleString("ko-KR", { style: "currency", currency: "KRW" })
+
+function changeValue(stringNumber, element) {
+  let tempString = stringNumber.replaceAll(",", "");    // value를 받아서 value값 안에 모든 ,를 빈칸으로 만들어준다.
+  let numberCash = BigInt(tempString);    // string을 int로 변환해주는데... BigInt는 2의 53제곱 보다 큰 값을 예상할 경우에 사용한다
+  element.value = numberCash      // BigInt로 형변환 해준 데이터를 선택한 요소의 value값에 할당한다.
+    .toLocaleString("ko-KR", { style: "currency", currency: "KRW" })     // 데이터를 현지날짜, 현지통화등으로 바꿔주는 메서드이다.
     .slice(1);
 }
+
+
 workElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,workElement)
+  changeValue(event.target.value, workElement)     // event.target.value는 이벤트(input)이 발생한 위치의 value
 );
 cashElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,cashElement)
+  changeValue(event.target.value, cashElement)
 );
 houseElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,houseElement)
+  changeValue(event.target.value, houseElement)
 );
 landElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,landElement)
+  changeValue(event.target.value, landElement)
 );
 medicalElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,medicalElement)
+  changeValue(event.target.value, medicalElement)
 );
 carElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,carElement)
+  changeValue(event.target.value, carElement)
 );
 debtElement.addEventListener("input", (event) =>
-  changeValue(event.target.value,debtElement)
+  changeValue(event.target.value, debtElement)
 );
-
