862d41c3b60497f919e6c0f9a38ba64b707cb558
yiju <kls9939@naver.com>
2022-05-12 15:54:28 +0900

    디자인 작업 중

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        print(request.POST)
+        print(request.POST.get('bokji-type'))
+        return redirect("/")
     return render(request, "landing/index.html")
