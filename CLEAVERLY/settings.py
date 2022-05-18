71ed3db7a7b6f9503b6109085fea3977f6551ae6
susan96322 <jkm0310@gmail.com>
2022-05-18 16:16:03 +0900

    코드 수정 무시

--- CLEAVERLY/settings.py
+++ CLEAVERLY/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing',
+    "allauth",
+    "django.contrib.sites",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.kakao",
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
@@ -130,3 +136,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+SITE_ID =1
