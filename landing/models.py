470262f378fd9cdcb3b37c6685d384a1d8e62048
susan96322 <jkm0310@gmail.com>
2022-05-18 11:28:58 +0900

    1. 복지추천 함수 생성 2. 데이터 저장 함수 생성 3. requirements.txt 수정- mysqlclient 설치 4. 번호 받아오는 함수 수정

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class UserDetail(models.Model):
+    user_type = models.CharField(max_length=50)
+    age = models.BigIntegerField(null=True)
+    family_number = models.BigIntegerField(null=True)
+    median_income = models.IntegerField(null=True)
+    median_income_value = models.IntegerField(null=True)
+    result_img = models.ImageField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class Welfare(models.Model):
+    welfare_name = models.CharField(max_length=40)
+    welfare_benefit = models.TextField(null=True)
+    welfare_link = models.TextField(null=True)
+    welfare_type = models.CharField(max_length=10, null=True)
+    welfare_condition = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserPhone(models.Model):
+    phone_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
